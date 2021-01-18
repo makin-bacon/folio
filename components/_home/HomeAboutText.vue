8f0c9805f7ccc197a37280951b8248189593237b
Tim <tm.devbits@gmail.com>
2021-01-19 09:11:58 +1100

    text edit

--- components/_home/HomeAboutText.vue
+++ components/_home/HomeAboutText.vue
@@ -33,9 +33,9 @@
         }"
         class="mb-12"
       >
-        While you already know me we do have a criteria to meet here so I've
-        made a section covering my vital statistics and work history. You can
-        also find a list of tools I use daily.
+        While you already know me we do have a criteria to meet so I've made a
+        section covering my vital statistics and work history. You can also find
+        a list of tools I use daily.
       </p>
       <NuxtLink
         v-gsap.from="{
---
+++ dist/_nuxt/static/1611007903/skills/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1611007903",layout:"default",error:null,serverRendered:a,routePath:"\u002Fskills",config:{},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"light"));
