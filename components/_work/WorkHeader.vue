da795ba75571e403112930cd24e4ccef877f246e
Tim <tm.devbits@gmail.com>
2021-01-19 13:58:20 +1100

    header fix x2
    about and work headers

--- components/_work/WorkHeader.vue
+++ components/_work/WorkHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-16 w-100 work-header-image">
+  <div class="mt-24 w-100 work-header-image">
     <div class="container pt-48 pb-12 mx-auto min-h-96 lg:pb-24 lg:pt-96"></div>
   </div>
 </template>
